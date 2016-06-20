cbe77646911ec125294c690c6137cd2ff8de617d
root <kpelesz@gmail.com>
2016-06-20 19:07:51 -0400

    Deletes useless tests

--- sslauto.sh
+++ sslauto.sh
@@ -4,5 +4,13 @@ list=$(cat $1)
 mkdir $2
 for i in $list; do
 echo "$i"
-OPENSSL=bin/openssl.Linux.x86_64 ./testssl.sh $i | aha > $2/$i-testssl
+OPENSSL=bin/openssl.Linux.x86_64 ./testssl.sh $i | aha > $2/$i-testssl.html
+file=$2/$i-testssl.html
+errgrep=$(grep "failed abc" $file)
+if [[ $errgrep == "failed abc" ]]
+then
+rm $file
+fi
 done
+
+
