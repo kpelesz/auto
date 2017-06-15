2d19a6db234030c5840afd76399bb7d9cbbff8bb
Katya Pelesz <kpelesz@gmail.com>
2017-06-15 15:22:01 -0400

    Create nccheck.sh

--- nccheck.sh
+++ nccheck.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
 #Requires aha to output the results into html file. 
-#nc iplist.txt | aha >file.html
-#Make sure to specify the file with the list of urls
+#nc iplist.txt
 
 #Future improvements - Use nmap xml file
-#Future improvements - Remove blank outputs
 #Future improvements - Multi-process?
 
 if [[ $# -eq 0 ]]; then
 	echo '	This will send a bunch of As to netcat based on a provided list of ips and ports.'
 	echo ''
-	echo '"nccheck iplist.txt | aha >file.html"'
+	echo '"nccheck iplist.txt"'
 	exit
 fi
 
 #Variables, future improvements - check the file exists
 
+valid=()
 list=$1
-touch a.txt | echo "aaaaaaaaaaaaaaaaaaaaaaaaaaa" > a.txt
+touch a.txt | echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa" > a.txt
 
 while read i; do
-		echo $i  
+		echo $i
 		timeout 10 nc $i < a.txt
+		exits=$?
 		echo ""
 		echo ""
-	done < $list
+		if [[ $exits -ne 124 ]]; then
+		valid+=("$i")
+		fi
+	done <$list
 
+
+run ()
+{
+for n in "${valid[@]}"; do
+		echo $n  
+		nc -w 10 $n < a.txt
+		echo ""
+		echo ""
+	done
+}
+run $valid | aha >$1-nccheck.html
